a68c653fd5b468b0e0a575240a02f06411913d5d
Vinu Cyril <vinucyril@gmail.com>
2023-11-22 19:58:41 +0530

    saving transactions in firebase, fetching data from db

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -5,13 +5,44 @@ import Header from "../components/Header";
 import Cards from "../components/Cards";
 import AddExpenseModal from "../components/modals/AddExpense";
 import AddIncomeModal from "../components/modals/AddIncome";
+import { auth, db } from "../firebase";
+import { addDoc, collection, getDocs, query } from "firebase/firestore";
+import { useAuthState } from "react-firebase-hooks/auth";
+import moment from "moment";
 
 const Dashboard = () => {
+  const [user] = useAuthState(auth);
   const [visible, setVisible] = useState(false);
   const [success, setSuccess] = useState(null);
   const [errorText, setErrorText] = useState("");
   const [isExpenseModalVisible, setIsExpenseModalVisible] = useState(false);
   const [isIncomeModalVisible, setIsIncomeModalVisible] = useState(false);
+  const [transactions, setTransactions] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const sampleTransactions = [
+    {
+      name: "Pay day",
+      type: "income",
+      date: "2023-01-15",
+      amount: 2000,
+      tag: "salary",
+    },
+    {
+      name: "Dinner",
+      type: "expense",
+      date: "2023-01-20",
+      amount: 500,
+      tag: "food",
+    },
+    {
+      name: "Books",
+      type: "expense",
+      date: "2023-01-25",
+      amount: 300,
+      tag: "education",
+    },
+  ];
 
   const showExpenseModal = () => {
     setIsExpenseModalVisible(true);
@@ -37,23 +68,63 @@ const Dashboard = () => {
       tag: values.tag,
       name: values.name,
     };
+    addTransaction(newTransaction);
+    setTransactions([...transactions, newTransaction]);
+    setIsExpenseModalVisible(false);
+    setIsIncomeModalVisible(false);
   };
 
-  const user = useSelector((state) => state.user.user);
-
-  useEffect(() => {
-    if (user) {
-      setErrorText(`Welcome to your DashBoard, ${user}`);
+  async function addTransaction(transaction) {
+    try {
+      const docRef = await addDoc(
+        collection(db, `users/${user.uid}/transactions`),
+        transaction
+      );
+      console.log("Document written with ID: ", docRef.id);
+      setErrorText(`Transaction added`);
       setSuccess(true);
       setVisible(true);
+    } catch (e) {
+      setErrorText(`Error: ${e}`);
+      setSuccess(false);
+      setVisible(true);
     }
+  }
+
+  async function fetchTransactions() {
+    setLoading(true);
+    if (user) {
+      const q = query(collection(db, `users/${user.uid}/transactions`));
+      const querySnapshot = await getDocs(q);
+      let transactionsArray = [];
+      querySnapshot.forEach((doc) => {
+        // doc.data() is never undefined for query doc snapshots
+        transactionsArray.push(doc.data());
+      });
+      setTransactions(transactionsArray);
+    }
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    setErrorText(`Welcome to your DashBoard, ${user}`);
+    setSuccess(true);
+    setVisible(true);
 
     setTimeout(() => {
       setErrorText("");
       setSuccess(null);
       setVisible(false);
     }, 2000);
-  }, []);
+  }, [user]);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setErrorText("");
+      setSuccess(null);
+      setVisible(false);
+    }, 2000);
+  }, [visible]);
 
   return (
     <>
